a948ef40b1e56d01eded07f0c11674a8c5662ae9
akash kumar <akashkumar63652@gmail.com>
2023-11-07 17:22:05 +0000

    comm-2

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-echo "code updated" > /tmp/code.txt
+#!/bin/bash
+echo "code updated" > /tmp/akash/code.txt
+mkdir -p /tmp/akash/{india/{delhi,delhi},aus/{a,b}}
+cat /etc/passwd >> /tmp/akash/user.txt
