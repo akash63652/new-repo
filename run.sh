251b43f8145bd9ebabb79a861c89678cd3274aa5
akash kumar <akashkumar63652@gmail.com>
2023-11-07 18:04:31 +0000

    add:12

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "code updated" > /tmp/akash/code.txt
-mkdir -p /tmp/akash/{india/{delhi,delhi},aus/{a,b}}
-cat /etc/passwd >> /tmp/akash/user.txt
+echo "new code has been updated" > /tmp/akash/code.txt
